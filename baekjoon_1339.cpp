8dd91a41ad8c695dc59f80c4629e277c6717caf9
유영훈 <yooyounghoon@MacBook-Air.local>
2021-10-03 16:18:11 +0900

    stop

--- baekjoon_1339.cpp
+++ baekjoon_1339.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
 int main(){
-    int a;
+    int N;
+    string str;
+    vector<string> str_vec;
+    for(int i=0;i<N;++i){
+        cin>>str;
+        str_vec.push_back(str);
+    }
+    
     return 0;
 }
