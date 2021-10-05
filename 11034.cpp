65f7006796a2937eaeae1ea8376fe9b7397e52c8
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-10-05 20:46:05 +0900

    11034

--- 11034.cpp
+++ 11034.cpp
@@ -4,17 +4,18 @@
 using namespace std;
 int main(){
     int N[3],mini,answer;
-    while(1){
-    for(int i=0;i<3;++i){
-        cin>>N[i];
-    }
+    // while(1){
+    // for(int i=0;i<3;++i){
+    //     cin>>N[i];
+    // }
+    while(cin >> N[0] >> N[1] >> N[2]){
     if(N[2]-N[1]>N[1]-N[0]){
         answer=N[2]-N[1]-1;
     }
     else{
         answer=N[1]-N[0]-1;
     }
-    cout<<answer;
+    cout<<answer<<endl;
     }
     return 0;
 }
