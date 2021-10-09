f95c84b1a940ec70d1c6f273dc2916c566088e22
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-10-09 20:25:21 +0900

    1193

---
+++ 1193.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    int N,count=1,total=0;
+    cin>>N;
+    while(total<N){
+        total+=count;
+        count++;
+    }
+    // cout<<total-N+1<<endl;
+    if(count%2){
+        cout<<count-(total-N+1)<<"/"<<total-N+1;
+    }
+    else{
+        cout<<total-N+1<<"/"<<count-(total-N+1);
+    }
+    return 0;
+}
