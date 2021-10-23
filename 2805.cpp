235fef832a95d4ab531ceb136b9092dfb083e477
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-10-23 14:30:34 +0900

    2805

---
+++ 2805.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+    long long  n,m;
+    long long  count=0,start=0;
+    cin>>n>>m;
+    vector<int> temp(n);
+    for(long long i=0;i<n;++i){
+        cin>>temp[i];
+    }
+    long long maxi = *max_element(temp.begin(),temp.end());
+    maxi--;
+    while(start<=maxi){
+        long long count=0;
+        long long mid = (start+maxi)/2;
+        for(auto i:temp){
+            i-mid<0 ? count+=0:count+=i-mid;
+        }
+        // cout<<count<<" ";
+        count<m ? maxi = mid-1:start=mid+1;
+    }
+    cout<<maxi;
+    return 0;
+}
