8ccccc9a47aa8c068649db5c5de9e81123104220
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-10-24 19:00:49 +0900

    17413

--- 17413.cpp
+++ 17413.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <vector>
 #include <stack>
 using namespace std;
 int main(){
@@ -10,24 +8,8 @@ int main(){
     string str;
     string buffer;
     getline(cin,str);
-    // istringstream ss(str);
-    // string buffer;
     stack<char> good;
     string temp;
-    // vector<string> temp;
-    // while(getline(ss,buffer,' ')){
-    //     temp.push_back(buffer);
-    //     count++;
-    // }
-    // for(int i=0;i<temp.size();++i){
-    //     reverse(temp[i].begin(),temp[i].end());
-    //     if(i==temp.size()-1){
-    //         cout<<temp[i];
-    //     }
-    //     else{
-    //         cout<<temp[i]<<" ";
-    //     }
-    // }
     for(char ch:str){
         if(ch=='<'){
             while(!good.empty()){
