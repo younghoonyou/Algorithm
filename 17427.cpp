1cd62721d8e382528d4184fa603d9ad28b8b77b6
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-11-12 21:28:08 +0900

    17427

--- 17427.cpp
+++ 17427.cpp
@@ -2,18 +2,21 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n;
+long long yaksu(int n){
     long long sum=0;
-    cin>>n;
     for(int i=1;i<=n;++i){
         long long temp = n / i;
         temp *= i;
         sum += temp;
     }
-    cout<<sum;
-    return 0;
+        return sum;
 }
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    cout<<yaksu(N);
+    return 0;
+}//초기화 시키는거 잊지말자 제발..
