dcdc61d38533fbe67c6955359b2e9a53e4260b94
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-11-12 19:47:34 +0900

    4375

---
+++ 4375.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long N;
+    while((cin>>N)){
+    long long count=1,t=1,sum=0;
+    while(t<=N){
+        t*=10;
+    }
+    long long b = t%N;
+    while(1){
+        sum+=b;
+        b = (10*b)%N;
+        if(sum%N==0){
+            break;
+        }
+        count++;
+    }
+    cout<<count<<'\n';
+    count=1;
+    }
+    return 0;
+}
