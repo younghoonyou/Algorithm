cd4c104ec66a9dcafc307c7fe5b0d8bba965bca6
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-11-16 20:07:51 +0900

    14501

---
+++ 14501.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int P[20];
+int T[20];
+int dp[20];
+int N;
+int consult(int n){
+    if(dp[n]){
+        return dp[n];
+    }
+    if(n>N){
+        return 0;
+    }
+    if(T[n]<=N-n+1){
+        dp[n] = P[n];
+        return dp[n] = max(consult(n+T[n])+P[n],consult(n+1));
+    }
+    else{
+        return  dp[n] = consult(n+1);
+    }
+}
+int main(){
+    int maxi=0;
+    cin>>N;
+    for(int i=1;i<=N;++i){
+        cin>>T[i]>>P[i];
+    }
+    for(int i=1;i<=N;++i){
+        if(maxi<consult(i)){
+            maxi = consult(i);
+        }
+    }
+    cout<<maxi;
+    return 0;
+}
