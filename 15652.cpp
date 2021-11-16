108e1bc3a5fdef33708711a9227629ae49686a1f
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-11-16 22:06:46 +0900

    15652

---
+++ 15652.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int N,M;
+bool v[12];
+int arr[12];
+void answer(int n){
+    if(n==M){
+        for(int i=0;i<M;++i){
+            cout<<arr[i]<<" ";
+        }
+        cout<<'\n';
+        return;
+    }
+    for(int i=1;i<=N;++i){
+        if(arr[n-1]<=i){
+            arr[n] = i;
+            answer(n+1);
+        }
+    }
+}
+int main(){
+    cin>>N>>M;
+    answer(0);
+    return 0;
+}
