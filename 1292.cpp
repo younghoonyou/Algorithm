b691120b0d1090043ce5f58339c7032efa6143d4
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-11-28 00:01:18 +0900

    1292

--- 1292.cpp
+++ 1292.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int arr[1001];
 int num(int n){
+    if(n==0) return 0;
     int sum=0;
     int i=1;
     while(n!=i-1){
@@ -15,14 +16,13 @@ int main(){
     int sum=0;
     cin>>a>>b;
     int i=1;
-    while(num(i)<=b){
+    while(num(i-1)<=b){
         for(int j=num(i-1)+1;j<=num(i);++j){
             arr[j] = i;
         }
         i++;
     }
     for(int k=a;k<=b;++k){
-        cout<<arr[k]<<" ";
         sum+=arr[k];
     }
     cout<<sum;
