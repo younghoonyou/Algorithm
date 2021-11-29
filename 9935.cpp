ec7f2e2423889266b18648f2ee19f097aef19b79
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-11-29 13:05:49 +0900

    9935

--- 9935.cpp
+++ 9935.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
-#include <stack>
 using namespace std;
 int main(){
-    string str,boom;
-    char past = ' ';
-    string temp;
-    cin>>str;
-    int count=0;
-    cin>>boom;
-    stack<char> s;
-    for(char ch:str){
-        int size = boom.size();
-        if(s.top()==boom[count]){
-            count++;
-            if(ch==boom[count]){
-                temp+=s.top();
+    string str,boom,temp;
+    cin>>str>>boom;
+    for(int i=0;i<str.length();++i){
+        int cnt=0;
+        int trial = boom.length();
+        temp+=str[i];
+        if(str[i]==boom[boom.length()-1]){
+            int j = 1;
+            while(trial--){
+                if(temp[temp.length()-j]==boom[trial]) cnt++;
+                j++;
             }
-            else{
-                temp.clear();
-                count=0;
+            if(cnt==boom.length()){
+                for(int k=0;k<boom.length();++k){
+                    temp.pop_back();
+                }
             }
-        }
-        s.push(ch);
-        if(temp==boom){
-            while(size--){
-                s.pop();
-            }
-            temp.clear();
-            count=0;
         }
     }
-        while(!s.empty()){
-            cout<<s.top();
-            s.pop();
-        }
+    if(temp.length()==0) cout<<"FRULA"<<'\n';
+    else cout<<temp<<'\n';
     return 0;
 }
