703686fcae7a6fbf0fd4d4562a989ff45af4263e
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-12-20 09:46:11 +0900

    1407

---
+++ 1407.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+ll a,b,answer;
+ll f(ll n){
+    ll r = n;
+    for(ll i=2;i<=n;i*=2){
+        r+=(n/i)*(i/2);
+    }
+    return r;
+}//F(n) = ∑(2^k-1)*(n/2^k)	k=0 ~ ∞ 공식을 알아야한다
+int main(){
+    ll sum = 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin>>a>>b;
+    cout<<f(b) - f(a-1);
+    return 0;
+}
