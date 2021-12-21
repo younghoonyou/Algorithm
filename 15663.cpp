b41caab5800f200b6ad21e6dea122f09ff1b70ea
younghoonyou <kcnoh2@hanyang.ac.kr>
2021-12-21 14:19:09 +0900

    15663

---
+++ 15663.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int N,M;
+int arr[8];
+int tmp[8];
+bool v[8];
+
+void back(int n){
+    if(n==M){
+        for(int i=0;i<M;++i){
+            cout<<tmp[i]<<" ";
+        }
+        cout<<'\n';
+        return ;
+    }
+    int vc = 0;
+    for(int i=0;i<N;++i){
+        if(!v[i] && arr[i]!=vc){
+            v[i] = true;
+            tmp[n] = arr[i];
+            vc = tmp[n];
+            back(n+1);
+            v[i] = false;
+        }
+    }
+}
+int main(){
+    cin>>N>>M;
+    for(int i=0;i<N;++i){
+        cin>>arr[i];
+    }
+    sort(arr,arr+N);
+    back(0);
+    return 0;
+}
