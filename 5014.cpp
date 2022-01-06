802feb8ecfb6ae921e0fe9341c9320fb01211db0
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-01-06 16:41:25 +0900

    5014

--- 5014.cpp
+++ 5014.cpp
@@ -1,30 +1,31 @@
+//https://www.acmicpc.net/problem/5014
 #include <iostream>
-#include <algorithm>
+#include <vector>
 #include <queue>
 using namespace std;
-int F,S,G,U,D;
-int v[1000001];
-void elevator(int x){
-    v[x] = 0;
+int answer[1000001];
+bool v[1000001];
+int main(){
+    int f,s,g,u,d;
+    cin>>f>>s>>g>>u>>d;
     queue<int> q;
-    q.push(x);
+    q.push(s);
+    v[s] = true;
+    answer[s] = 0;
     while(!q.empty()){
         int cur = q.front();
+        if(cur == g){cout<<answer[g];return 0;}
         q.pop();
-        if(cur==G){cout<<v[cur];return ;}
         for(int i=0;i<2;++i){
             int nx;
-            if(i==0) nx = cur+U;
-            else nx = cur-D;
-            if(nx>F||v[nx]>0||nx<=0) continue;
+            if(i==0) nx = cur+u;
+            else nx = cur-d;
+            if(nx<1||nx>f||v[nx]) continue;
+            answer[nx] = answer[cur] + 1;
+            v[nx] = true;
             q.push(nx);
-            v[nx] = v[cur]+1;
         }
     }
     cout<<"use the stairs";
-}
-int main(){
-    cin>>F>>S>>G>>U>>D;
-    elevator(S);
     return 0;
 }
